d4a579cb6bfde5b59302e31662aac3c7de1856ae
nixpig <143995476+nixpig@users.noreply.github.com>
2025-04-06 06:58:29 +0100

    chore: update notes in readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # 🖥️ virtui
 
 An interactive Terminal UI (TUI) for managing QEMU/KVM virtual machines via libvirt. 
+
+## 🎯 Initial project goals
+
+Create a domain with CPU, memory, network and disk, from an ISO.
+
+
+## Notes
+
+1. Install packages: `pacman -Sy fuse3 libvirt radvd qemu-base virt-manager dnsmasq`
+1. Enable and start: `sudo systemctl enable --now libvirtd.service`
+1. Uncomment in: `/etc/libvirt/libvirtd.conf`
+    - `unix_sock_group = "libvirt"`
+    - `unix_sock_rw_perms = "0770"`
+1. Add user to group: `sudo usermod -aG libvirt $USER`
+
--- cmd/virtui.go
+++ cmd/virtui.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"libvirt.org/go/libvirt"
+)
 
 func main() {
-	fmt.Println("main virtui cmd")
+	conn, err := libvirt.NewConnect("qemu:///system")
+	if err != nil {
+		fmt.Println("new connection: ", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	fmt.Println("connected")
+
+	n, _ := conn.NumOfDomains()
+	fmt.Println("num domains: ", n)
+
+	d, _ := conn.LookupDomainById(1)
+	i, _ := d.GetUUIDString()
+	fmt.Println(i)
+
+	// doms, err := conn.GetAllDomainStats()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Println(doms)
 }

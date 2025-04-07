16280bd34eb50b791d635a7e93b2994a4a9b30a6
nixpig <143995476+nixpig@users.noreply.github.com>
2025-04-07 06:32:18 +0100

    chore: wip

--- README.md
+++ README.md
@@ -10,9 +10,31 @@ Create a domain with CPU, memory, network and disk, from an ISO.
 ## Notes
 
 1. Install packages: `pacman -Sy fuse3 libvirt radvd qemu-base virt-manager dnsmasq`
-1. Enable and start: `sudo systemctl enable --now libvirtd.service`
+1. Enable and start service: `sudo systemctl enable --now libvirtd.service`
 1. Uncomment in: `/etc/libvirt/libvirtd.conf`
     - `unix_sock_group = "libvirt"`
     - `unix_sock_rw_perms = "0770"`
+1. Set `firewall_backend=iptables` in `/etc/libvirt/network.conf`
 1. Add user to group: `sudo usermod -aG libvirt $USER`
+1. Enable polling and stats for memory etc... via virt-manager.
+
+
+## Commands from list
+
+- Select domain - ↓/↑ j/k
+- Open          - O
+- Run           - ENTER
+    - Pause         - P
+    - Shutdown      - S
+    - Reboot        - R
+    - Force Reset   - O
+    - Force Off     - F
+    - Save          - V
+    - Close menu    - ESC
+- Migrate       - M
+- Delete        - D
+- Clone         - C
+- Help          - H
+- New           - N
+- Quit          - Q/ESC
 
--- cmd/virtui.go
+++ cmd/virtui.go
@@ -4,30 +4,153 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/x/term"
 	"libvirt.org/go/libvirt"
 )
 
+const libvirtURI = "qemu:///system"
+
+var domainState = map[libvirt.DomainState]string{
+	libvirt.DOMAIN_NOSTATE:     "None",
+	libvirt.DOMAIN_RUNNING:     "Running",
+	libvirt.DOMAIN_BLOCKED:     "Blocked",
+	libvirt.DOMAIN_PAUSED:      "Paused",
+	libvirt.DOMAIN_SHUTDOWN:    "Shutdown",
+	libvirt.DOMAIN_CRASHED:     "Crashed",
+	libvirt.DOMAIN_PMSUSPENDED: "Suspended",
+	libvirt.DOMAIN_SHUTOFF:     "Shutoff",
+}
+
+func fmtState(d *libvirt.Domain) string {
+	s, _, err := d.GetState()
+	if err != nil {
+		return "-"
+	}
+	state, ok := domainState[s]
+	if !ok {
+		return "-"
+	}
+	return state
+}
+
+func fmtID(d *libvirt.Domain) string {
+	id := "-"
+	s, _, err := d.GetState()
+	if err != nil {
+		return "-"
+	}
+	if s != libvirt.DOMAIN_SHUTOFF {
+		u, _ := d.GetID()
+		id = fmt.Sprintf("%d", u)
+	}
+
+	return id
+}
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+type model struct {
+	table table.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			if m.table.Focused() {
+				m.table.Blur()
+			} else {
+				m.table.Focus()
+			}
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			return m, tea.Batch(
+				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
+			)
+		}
+	}
+
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return baseStyle.Render(m.table.View()) + "\n"
+}
+
 func main() {
-	conn, err := libvirt.NewConnect("qemu:///system")
+	conn, err := libvirt.NewConnect(libvirtURI)
 	if err != nil {
 		fmt.Println("new connection: ", err)
 		os.Exit(1)
 	}
 	defer conn.Close()
-	fmt.Println("connected")
 
-	n, _ := conn.NumOfDomains()
-	fmt.Println("num domains: ", n)
+	flags := libvirt.ConnectListAllDomainsFlags(libvirt.CONNECT_LIST_DOMAINS_ACTIVE | libvirt.CONNECT_LIST_DOMAINS_INACTIVE)
+	domains, _ := conn.ListAllDomains(flags)
 
-	d, _ := conn.LookupDomainById(1)
-	i, _ := d.GetUUIDString()
-	fmt.Println(i)
+	w, _, err := term.GetSize(0)
+	if err != nil {
+		fmt.Println("failed to get term size: ", err)
+	}
 
-	// doms, err := conn.GetAllDomainStats()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
+	idWidth := 4
+	stateWidth := 8
+	nameWidth := w - idWidth - stateWidth - 4 - 4 - 2
 
-	// fmt.Println(doms)
+	columns := []table.Column{
+		{Title: "ID", Width: idWidth},
+		{Title: "Name", Width: nameWidth},
+		{Title: "State", Width: stateWidth},
+	}
+
+	rows := make([]table.Row, len(domains))
+
+	for i, d := range domains {
+		name, _ := d.GetName()
+		state := fmtState(&d)
+		id := fmtID(&d)
+
+		rows[i] = table.Row{id, name, state}
+	}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+	)
+
+	s := table.DefaultStyles()
+
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+
+	t.SetStyles(s)
+
+	m := model{t}
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("failed to run program: ", err)
+		os.Exit(1)
+	}
 }

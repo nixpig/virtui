01c66720c8f55bb308e1d25c09081428ea132daa
nixpig <143995476+nixpig@users.noreply.github.com>
2025-04-14 04:13:14 +0100

    chore: wip

--- connection/connection.go
+++ connection/connection.go
@@ -4,31 +4,26 @@ import "database/sql"
 
 type Connection struct {
 	ID          int
-	Hypervisor  string
 	URL         string
 	Autoconnect bool
-	SSH         bool
-	Hostname    string
-	Username    string
-	Password    string
 }
 
-type ConnectionRepository interface {
+type ConnectionStore interface {
 	HasConnections() (bool, error)
 	InsertConnection(connection *Connection) error
 	GetConnections() ([]Connection, error)
-	DeleteConnection(id int) error
+	DeleteConnectionByID(id int) error
 }
 
-type ConnectionRepositoryImpl struct {
+type ConnectionStoreImpl struct {
 	db *sql.DB
 }
 
-func NewConnectionRepositoryImpl(db *sql.DB) ConnectionRepository {
-	return &ConnectionRepositoryImpl{db}
+func NewConnectionStoreImpl(db *sql.DB) ConnectionStore {
+	return &ConnectionStoreImpl{db}
 }
 
-func (c ConnectionRepositoryImpl) HasConnections() (bool, error) {
+func (c ConnectionStoreImpl) HasConnections() (bool, error) {
 	query := `select count(*) from connections_;`
 
 	var count int
@@ -40,34 +35,19 @@ func (c ConnectionRepositoryImpl) HasConnections() (bool, error) {
 	return count > 0, nil
 }
 
-func (c ConnectionRepositoryImpl) InsertConnection(connection *Connection) error {
+func (c ConnectionStoreImpl) InsertConnection(connection *Connection) error {
 	query := `insert into connections_ (
-		hypervisor_,
 		url_,
-		autoconnect_, 
-		ssh_,
-		hostname_,
-		username_,
-		password_
+		autoconnect_
 	) values (
-		$hypervisor,
 		$url,
-		$autoconnect,
-		$ssh,
-		$hostname,
-		$username,
-		$password
+		$autoconnect
 	);`
 
 	if _, err := c.db.Exec(
 		query,
-		sql.Named("hypervisor", connection.Hypervisor),
 		sql.Named("url", connection.URL),
 		sql.Named("autoconnect", connection.Autoconnect),
-		sql.Named("ssh", connection.SSH),
-		sql.Named("hostname", connection.Hostname),
-		sql.Named("username", connection.Username),
-		sql.Named("password", connection.Password),
 	); err != nil {
 		return err
 	}
@@ -75,9 +55,8 @@ func (c ConnectionRepositoryImpl) InsertConnection(connection *Connection) error
 	return nil
 }
 
-func (c ConnectionRepositoryImpl) GetConnections() ([]Connection, error) {
-	query := `select id_, hypervisor_, url_, autoconnect_, ssh_, hostname_, username_, password_ 
-		from connections_;`
+func (c ConnectionStoreImpl) GetConnections() ([]Connection, error) {
+	query := `select id_, url_, autoconnect_ from connections_;`
 
 	rows, err := c.db.Query(query)
 	if err != nil {
@@ -87,7 +66,11 @@ func (c ConnectionRepositoryImpl) GetConnections() ([]Connection, error) {
 	var connections []Connection
 	for rows.Next() {
 		var connection Connection
-		if err := rows.Scan(&connection); err != nil {
+		if err := rows.Scan(
+			&connection.ID,
+			&connection.URL,
+			&connection.Autoconnect,
+		); err != nil {
 			return nil, err
 		}
 
@@ -97,7 +80,7 @@ func (c ConnectionRepositoryImpl) GetConnections() ([]Connection, error) {
 	return connections, nil
 }
 
-func (c ConnectionRepositoryImpl) DeleteConnection(id int) error {
+func (c ConnectionStoreImpl) DeleteConnectionByID(id int) error {
 	query := `delete from connections_ where id_ = $id;`
 
 	if _, err := c.db.Exec(query, sql.Named("id", id)); err != nil {
